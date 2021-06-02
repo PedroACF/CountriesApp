25e292b441d3df9227f1474f069f41bfb682fc45
Pedro Alfonzo Condori Fernandez <pedro.acf.3@gmail.com>
2021-06-02 19:46:49 -0400

    completed countries

--- src/app/country/pages/show-country/show-country.component.ts
+++ src/app/country/pages/show-country/show-country.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {CountryService} from "../../services/country.service";
 import {switchMap} from "rxjs/internal/operators/switchMap";
+import {Country} from "../../interfaces/country.interface";
+import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-show-country',
@@ -11,16 +13,19 @@ import {switchMap} from "rxjs/internal/operators/switchMap";
 })
 export class ShowCountryComponent implements OnInit {
 
+  country!: Country;
+
   constructor(private activatedRoute: ActivatedRoute, private countryService: CountryService) { }
 
   ngOnInit(): void {
 
     this.activatedRoute.params
       .pipe(
-        switchMap(({code}) => this.countryService.country(code) )
+        switchMap(({code}) => this.countryService.country(code) ),
+        tap(console.log)
       )
       .subscribe(country=>{
-        console.log(country);
+        this.country = country;
       })
   }
 
